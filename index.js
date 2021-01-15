e76672715598d3cdde2afa660208748e427e32b6
Alec Scully <ScullyAlec@gmail.com>
2021-01-14 23:23:36 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeCheck = parseInt(time.split(":"))
+  // debugger
+  if (timeCheck < 12) {
+    return "Good Morning"
+  } else if ( 12 < timeCheck && timeCheck < 17) {
+    return "Good Afternoon"
+  } else if (timeCheck > 17) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  // debugger
+  document.querySelector("#greeting").innerText = string
+
+}
